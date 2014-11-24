e4eeac41161f1db8c8a4f9119c4a475d3bc78f89
Hanna Ai <hangha1812@gmail.com>
2014-11-23 21:32:15 -0500

    adding visual color and google glass scripts

---
+++ AiportHelper/Assets/CS4590 Final Project/Scripts/GoogleGlass.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoogleGlass : MonoBehaviour {
+	private GUIStyle currentStyle = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		
+	}
+	void OnGUI () {
+
+		float glassH = (Screen.height / 100) * 30;
+		float glassW = (Screen.width / 100) * 40;
+		float spaceW = Screen.width - (glassW + 10);
+		print (glassW);
+		// Make a background box
+		currentStyle = new GUIStyle (GUI.skin.box);
+		currentStyle.normal.background = makeTextureForBox (Color.white); // makes a box style class, 
+		// then you attach it to the box object. You can also make text styles and so on, and attach
+		// to the corresponding objects
+
+
+//		GUI.contentColor = Color.yellow; <--text color
+//		GUI.color = Color.white;
+		GUI.Box (new Rect (spaceW,10,glassW, glassH), "Say Ok Google to start", currentStyle);
+
+	}
+	private Texture2D makeTextureForBox( Color col )
+	{
+		col.a = 0.5f;// added this to control transparency, 0.1f = 10%
+		Texture2D result = new Texture2D( 1, 1 );
+		Color[] px = new Color[1];
+		px [0] = col;
+		result.SetPixels( px );
+		result.Apply();
+		return result;
+	}
+}
